be4133155d067698d0f61085bba7e4df23b4be01
Jatin Shukla <jatinshukla.lek@gmail.com>
2024-08-29 08:15:23 +0100

    Light mode issue fix

--- ui/src/components/image-info/ImageInfo.js
+++ ui/src/components/image-info/ImageInfo.js
@@ -9,11 +9,11 @@ const ImageInfo = ({imageData}) => {
     {imageData.attackedTable ?
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
             <Grid item xs={6} sx={{padding: '10px'}}>
-                <Typography variant='body1' color="white">Original Image:</Typography>
+                <Typography variant='body1'>Original Image:</Typography>
                 <ImageInfoTable tableData={imageData.originalTable} />
             </Grid>
             <Grid item xs={6} sx={{padding: '10px'}}>
-                <Typography variant='body1' color="white">Attacked Image:</Typography>
+                <Typography variant='body1'>Attacked Image:</Typography>
                 <ImageInfoTable tableData={imageData.attackedTable} />
             </Grid>
         </Grid> :
